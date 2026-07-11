d7fca00680ed40e08cb6790183c696c9b08ac32f
Otniel Mata <otnielmata@MacBook-Pro-de-Otniel.local>
2026-07-11 10:40:58 -0300

    feat: implement collaborator query flow

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,20 @@ import { Router } from "express";
 import { userController } from "../controllers/user.controller.js";
 import { adminMiddleware } from "../middleware/admin.middleware.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import { requirePermission } from "../middleware/permission.middleware.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
 export const userRoutes = Router();
 
 userRoutes.post("/bootstrap-admin", asyncHandler(userController.createBootstrapAdmin.bind(userController)));
+userRoutes.get("/me", authMiddleware, asyncHandler(userController.getAuthenticatedUser.bind(userController)));
+userRoutes.get("/", authMiddleware, asyncHandler(requirePermission("USER_LIST")), asyncHandler(userController.list.bind(userController)));
+userRoutes.get(
+  "/:id",
+  authMiddleware,
+  asyncHandler(requirePermission("USER_READ")),
+  asyncHandler(userController.getById.bind(userController))
+);
 userRoutes.post(
   "/",
   authMiddleware,
