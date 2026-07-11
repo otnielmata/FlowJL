a095228c28f4febf0cc6d4b95dc3e384540cd98d
Otniel Mata <otnielmata@MacBook-Pro-de-Otniel.local>
2026-07-11 11:04:02 -0300

    feat: implementar permissoes por cargo

--- src/routes/role.routes.js
+++ src/routes/role.routes.js
@@ -9,5 +9,17 @@ import { asyncHandler } from "../utils/async-handler.js";
 export const roleRoutes = Router();
 
 roleRoutes.get("/", authMiddleware, asyncHandler(requirePermission("ROLE_READ")), asyncHandler(roleController.list.bind(roleController)));
+roleRoutes.get(
+  "/:code/permissions",
+  authMiddleware,
+  asyncHandler(requirePermission("ROLE_READ")),
+  asyncHandler(roleController.getPermissions.bind(roleController))
+);
 roleRoutes.post("/", authMiddleware, asyncHandler(adminMiddleware), asyncHandler(roleController.create.bind(roleController)));
 roleRoutes.put("/:code", authMiddleware, asyncHandler(adminMiddleware), asyncHandler(roleController.update.bind(roleController)));
+roleRoutes.put(
+  "/:code/permissions",
+  authMiddleware,
+  asyncHandler(adminMiddleware),
+  asyncHandler(roleController.updatePermissions.bind(roleController))
+);
