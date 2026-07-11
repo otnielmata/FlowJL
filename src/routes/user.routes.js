e980f0eea94daf72a552b735ae97e6537d6e7c0d
Otniel Mata <otnielmata@MacBook-Pro-de-Otniel.local>
2026-07-11 10:25:38 -0300

    feat: implement collaborator creation flow

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
 import { userController } from "../controllers/user.controller.js";
+import { adminMiddleware } from "../middleware/admin.middleware.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
 export const userRoutes = Router();
 
 userRoutes.post("/bootstrap-admin", asyncHandler(userController.createBootstrapAdmin.bind(userController)));
+userRoutes.post(
+  "/",
+  authMiddleware,
+  asyncHandler(adminMiddleware),
+  asyncHandler(userController.createCollaborator.bind(userController))
+);
 userRoutes.put("/:id", authMiddleware, asyncHandler(userController.update.bind(userController)));
