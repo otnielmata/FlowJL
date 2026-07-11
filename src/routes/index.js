6ad2c7714fd073d2a8172a8b81d4a7cfc229ce58
Otniel Mata <otnielmata@MacBook-Pro-de-Otniel.local>
2026-07-11 17:37:08 -0300

    feat: implement FJ-30 reels flow

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import { authRoutes } from "./auth.routes.js";
 import { contentIdeaRoutes } from "./content-idea.routes.js";
 import { launchRoutes } from "./launch.routes.js";
 import { profileRoutes } from "./profile.routes.js";
+import { reelRoutes } from "./reel.routes.js";
 import { roleRoutes } from "./role.routes.js";
 import { userRoutes } from "./user.routes.js";
 
@@ -14,7 +15,7 @@ apiRoutes.get("/", (_request, response) => {
   response.status(200).json({
     service: "flow-jl-api",
     version: "v1",
-    modules: ["audits", "auth", "content-ideas", "launches", "profiles", "roles", "users", "swagger"]
+    modules: ["audits", "auth", "content-ideas", "launches", "profiles", "reels", "roles", "users", "swagger"]
   });
 });
 
@@ -23,5 +24,6 @@ apiRoutes.use("/auth", authRoutes);
 apiRoutes.use("/content-ideas", contentIdeaRoutes);
 apiRoutes.use("/launches", launchRoutes);
 apiRoutes.use("/profiles", profileRoutes);
+apiRoutes.use("/reels", reelRoutes);
 apiRoutes.use("/roles", roleRoutes);
 apiRoutes.use("/users", userRoutes);
